d47fe3be0d949e2454bff17321c365726edeea95
Brian Bycynski <high5bri@hotmail.com>
2017-02-11 09:22:26 -0500

    added google firebase

---
+++ src/base.js
@@ -0,0 +1,8 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCLBv_3s64O2eE2YSXwXNp1lMxZtZobZ9s",
+      authDomain: "catch-of-the-day-brianbycynski.firebaseapp.com",
+      databaseURL: "https://catch-of-the-day-brianbycynski.firebaseio.com",
+});
+export default base;
